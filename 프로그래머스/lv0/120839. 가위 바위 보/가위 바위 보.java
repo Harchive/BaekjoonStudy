e159fb3823cb1767540e5897bbdf3cdc251e9231
Harchive <76925525+Harchive@users.noreply.github.com>
2023-07-20 15:28:31 +0900

    [level 0] Title: 가위 바위 보, Time: 2.20 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        String[] arr = rsp.split("");
+        for(int i = 0; i<arr.length; i++){
+            switch(arr[i]){
+                case "2" : 
+                    answer += "0";
+                    break;
+                case "0" :
+                    answer += "5";
+                    break;
+                default :
+                    answer += "2";
+                    break;    
+                    
+            }
+        }
+        return answer;
+    }
+}
