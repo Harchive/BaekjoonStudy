b22104f791ca98ecfcca524fe68a006536a88484
Harchive <76925525+Harchive@users.noreply.github.com>
2023-07-28 15:45:34 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.65 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String my_string) {
+        String[] arr = {};
+        my_string = my_string.replaceAll("[^0-9]","");
+        arr = my_string.split("");
+        
+        int[] answer = new int[arr.length];
+        
+        for(int i=0; i<arr.length;i++){
+            answer[i] = Integer.parseInt(arr[i]);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
