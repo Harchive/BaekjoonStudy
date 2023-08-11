c0593d492bb2c179c34b83a23ef8e2c1ec5b2137
Harchive <76925525+Harchive@users.noreply.github.com>
2023-08-11 16:01:27 +0900

    [level 0] Title: 가까운 수, Time: 0.36 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        Arrays.sort(array);
+        int answer = 0;
+        int num = n+100;
+        
+        for (int i = 0; i < array.length; i++) {
+          if (Math.abs(array[i] - n) < num) {
+            num = Math.abs(array[i] - n);
+            answer = i;
+          }
+        }
+        return array[answer];
+    }
+}
