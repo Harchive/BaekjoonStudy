22ad79796c23a4a1393d5c6a74ac695b0ecd73ce
Harchive <76925525+Harchive@users.noreply.github.com>
2023-08-16 10:41:56 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 1.44 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split("");
+        
+        Arrays.sort(arr);
+        int num = 0;
+        
+        for(int i=0; i<arr.length; i++) {
+            num = 0;
+            for(int j = 0; j< arr.length;j++){
+                if(arr[i].equals(arr[j])){
+                    num++;
+                }
+            }
+            if(num == 1){
+                answer += arr[i];
+            }
+        }
+        
+        return answer;
+    }
+}
