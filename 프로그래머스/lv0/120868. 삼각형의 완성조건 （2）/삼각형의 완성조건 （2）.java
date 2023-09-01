d78da0701485c8ca02549885ce388bff65c5a0c3
Harchive <76925525+Harchive@users.noreply.github.com>
2023-09-01 10:46:59 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.38 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        Arrays.sort(sides);
+        answer = sides[0];
+        answer += sides[0]-1;
+        return answer;
+    }
+}
