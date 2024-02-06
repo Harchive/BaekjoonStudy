1e083517fa66cfd6cf30d6e27c7711341a4fe635
Harchive <76925525+Harchive@users.noreply.github.com>
2024-02-06 13:39:42 +0900

    Update 이진 변환 반복하기.java

--- 프로그래머스/2/70129. 이진변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진변환 반복하기/이진 변환 반복하기.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        int num = 0;
+        int count = 0;
+        int sum = 0;
+        int zero = 0;
+            
+        while(!s.equals("1")){
+            count++; 
+            zero = s.length();
+            s = s.replaceAll("0","");
+            num = s.length();
+            sum += (zero - num);
+            s = Integer.toBinaryString(num);
+        }
+            answer[0] = count;
+            answer[1] = sum;
+        return answer;
+    }
+}
