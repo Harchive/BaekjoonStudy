7dfaa26da848bbb3aeeefb1acaccb21dde7906ef
Harchive <76925525+Harchive@users.noreply.github.com>
2024-02-21 12:30:11 +0900

    Update 점프와 순간 이동.java

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -1,1 +1,15 @@
+import java.util.*;
 
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        String str = Integer.toBinaryString(n);
+        for(int i = 0; i<str.length();i++){
+            if(str.charAt(i) == '1'){
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
